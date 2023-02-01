da29b47d70967899eb4281d3b0f197e69a89d18b
issach315 <117745188+issach315@users.noreply.github.com>
2023-02-01 11:17:25 +0530

    iterations

--- Program1.java
+++ Program1.java
@@ -20,10 +20,27 @@ public class Program1
         System.out.println();
     }
     /*  int i=1 i<=5 condition :true
+            
+            int j=1 ; j<=5 condition :true
+                
         int i=2 i<=5 condition :true
+        
+             int j=2 ; j<=5 condition :true
+             
         int i=3 i<=5 condition :true
+            
+             int j=3 ; j<=5 condition :true
+             
+             
         int i=4 i<=5 condition :true
+        
+             int j=4 ; j<=5 condition :true
+             
         int i=5 i<=5 condition :true
+            
+             int j=5 ; j<=5 condition :true
+             
+             
         int i=6 i<=5 condition :false
      
      */
